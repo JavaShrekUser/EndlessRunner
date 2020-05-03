921e899977436edffd7beb2e7d1049f17350d92a
JavaShrekUser <63369013+JavaShrekUser@users.noreply.github.com>
2020-05-03 15:14:43 -0700

    Add files via upload

--- src/scenes/BgStory.js
+++ src/scenes/BgStory.js
@@ -11,13 +11,16 @@ class BgStory extends Phaser.Scene {
         this.load.image('mainBack', './assets/mainback.png');
         this.load.image('textBack', './assets/textBack.png');
         this.load.audio('choco','./assets/chocobirdsrun.mp3');
+        this.load.image('storyBack', './assets/storyBack.png');
+        this.load.image('textBack', './assets/textBack.png');
+
     }
 
     create(){
         //bgStory display
         let bgStoryConfig = {
             fontFamily: 'Courier',
-            fontSize: '28px',
+            fontSize: '22px',
             backgroundColor: '#00000000',
             color: '#E0FFF6',
             align:'right',
@@ -28,13 +31,13 @@ class BgStory extends Phaser.Scene {
             fixedWidth:0
         }
 
-        let titleConfig = {
-            fontFamily: 'fantasy',
-            fontSize: '27px',
+        let redConfig = {
+           fontFamily: 'Courier',
+            fontSize: '24px',
             backgroundColor: '#00000000',
-            color: '#E0FFF6',
-            align: 'right',
-            padding: {
+            color: '#FFF701',
+            align:'right',
+            padding:{
                 top:5,
                 bottom:5,
             },
@@ -44,16 +47,27 @@ class BgStory extends Phaser.Scene {
         //show bgStory text
         let centerX = game.config.width/2;
         let centerY = game.config.height/2;
-        let textSpacer = 64;
+        let textSpacer = 29;
 
 
         this.mainBack = this.add.tileSprite(0, 0, 680, 480, 'mainBack').setOrigin(0,0);
         // this.UI =  this.add.tileSprite(centerX, centerY - 130, 551, 64, 'UI old').setOrigin(0.5);
-        this.textBack =  this.add.tileSprite(centerX, 270, 624, 130, 'textBack').setOrigin(0.5);
+        this.storyBack =  this.add.tileSprite(0, 0, 640, 480, 'storyBack').setOrigin(0.0);
 
         // this.add.text(centerX, centerY - 130, 'Where is my play yard', titleConfig).setOrigin(0.5);
-        this.add.text(centerX, centerY, '小朋友，你是否有很多问号', bgStoryConfig).setOrigin(0.5);
-        this.add.text(centerX, centerY + textSpacer, 'Press → to start', bgStoryConfig).setOrigin(0.5);
+        this.add.text(centerX, centerY - textSpacer*6, 'Story: You are in a swimming competition, but', bgStoryConfig).setOrigin(0.5);
+        this.add.text(centerX, centerY - textSpacer*5, 'soon you lost your way. You did not find that', bgStoryConfig).setOrigin(0.5);
+        this.add.text(centerX-7, centerY - textSpacer*4, ' you are lost, and the only thing in your mind', bgStoryConfig).setOrigin(0.5);
+        this.add.text(centerX-7, centerY - textSpacer*3, ' is the desire to win. However, as your speed ', bgStoryConfig).setOrigin(0.5);
+        this.add.text(centerX, centerY - textSpacer*2, 'increases, you find more and more garbage and', bgStoryConfig).setOrigin(0.5);
+        this.add.text(centerX, centerY - textSpacer*1, 'fishing nets around you. Finally, you realize', bgStoryConfig).setOrigin(0.5); //'Press → to start'
+        this.add.text(centerX, centerY,                'that you are in a big trouble....', bgStoryConfig).setOrigin(0.5);
+
+        this.add.text(centerX, centerY + 62, 'Control: Use arrow keys to move.', redConfig).setOrigin(0.5);
+
+        this.add.text(centerX, centerY + 127, 'Avoid All Fishing Tools and Garbages.', redConfig).setOrigin(0.5);
+
+        this.add.text(centerX, centerY + 192, 'Press → to start.', redConfig).setOrigin(0.5);
         
         // define keys
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
